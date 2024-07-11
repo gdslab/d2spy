7a0535bc51316b036c5a356d3fd8c9aef1dfaba8
Ben Hancock <hancocb@purdue.edu>
2024-07-11 13:56:58 -0400

    drop removed property from project schema

--- d2spy/schemas/project.py
+++ d2spy/schemas/project.py
@@ -9,35 +9,33 @@ from d2spy.schemas.geojson import GeoJSON
 @dataclass
 class Project:
     id: UUID
-    title: str
+    deactivated_at: Optional[datetime]
     description: str
-    planting_date: Optional[date]
+    field: GeoJSON
+    flight_count: int
     harvest_date: Optional[date]
     is_active: bool
     location_id: UUID
+    planting_date: Optional[date]
+    role: Literal["owner", "manager", "viewer"]
     team_id: UUID
-    deactivated_at: Optional[datetime]
-    owner_id: UUID
-    is_owner: bool
-    field: GeoJSON
-    flight_count: int
+    title: str
 
     @classmethod
     def from_dict(cls, data: dict) -> "Project":
         return cls(
             id=data["id"],
-            title=data["title"],
+            deactivated_at=data["deactivated_at"],
             description=data["description"],
-            planting_date=data["planting_date"],
+            field=data["field"],
+            flight_count=data["flight_count"],
             harvest_date=data["harvest_date"],
             is_active=data["is_active"],
             location_id=data["location_id"],
+            planting_date=data["planting_date"],
+            role=data["role"],
             team_id=data["team_id"],
-            deactivated_at=data["deactivated_at"],
-            owner_id=data["owner_id"],
-            is_owner=data["is_owner"],
-            field=data["field"],
-            flight_count=data["flight_count"],
+            title=data["title"],
         )
 
 
